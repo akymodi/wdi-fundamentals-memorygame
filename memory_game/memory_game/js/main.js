24ed1ae21c99c273336e94d2ad70e90fbfbe9d6f
Akash Modi <expese@akashs-mbp.lan>
2019-10-22 20:44:43 -0400

    created game logic

--- memory_game/memory_game/js/main.js
+++ memory_game/memory_game/js/main.js
@@ -1,18 +1,25 @@
 
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-	
-	var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log("User flipped " + cardOne);
 
-	var cardTwo = cards[2];
-	cardsInPlay.push(cardTwo);
-	console.log("User flipped " + cardTwo);
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+	}
+}
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert('You found a match!');
-}  
- 	else {
- 	alert('Sorry, try again');
+var flipcard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[0]);
+	if (cardsInPlay.length === 2){
+
+		checkForMatch();
+	}
+
  }
+
+flipcard(0);
+flipcard(2);
+
